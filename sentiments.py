17ee65bb40c6a409ad2bd517de923f2dc05ac9d0
Cole Holcomb <cholcomb@astro.princeton.edu>
2018-02-18 14:38:17 -0500

    sentiments.py: Naive Bayes makes predictions on test data.

--- sentiments.py
+++ sentiments.py
@@ -1,4 +1,5 @@
 #%%
+import numpy as np
 class NaiveBayes():
     '''
 
@@ -14,37 +15,65 @@ class NaiveBayes():
         self.nclasses = np.unique(labels).shape[0]
         self.nexamples = features.shape[0]
         self.nfeatures = features.shape[1]
-        self.class_priors = np.zeros(self.nclasses)
-        self.log_likelihood = np.zeros((self.nclasses, self.nfeatures))
+        self.class_priors = np.zeros(self.nclasses) + self.nclasses
+        self.likelihood = np.zeros((self.nclasses, self.nfeatures)) + 1
 
         for i in range(self.nexamples):
             y = int(self.labels[i])
             self.class_priors[y] += 1
             for j in range(self.nfeatures):
                 if self.features[i,j] == 1:
-                    self.log_likelihood[y,j] += 1 
+                    self.likelihood[y,j] += 1 
         self.class_priors = self.class_priors/self.nexamples
-        self.log_likelihood = self.log_likelihood/self.nexamples
+        self.likelihood = self.likelihood/self.nexamples
 
         print('N_features = {}'.format(self.nfeatures))
         print('N_classes  = {}'.format(self.nclasses))
         print('N_examples = {}'.format(self.nexamples))
 
         print(self.class_priors)
-        print(self.log_likelihood)
+        print(self.likelihood)
     
     def test(self, features, labels):
+        features = features.values
+        ntest = labels.shape[0]
+        pc = np.empty((ntest, self.nclasses))
         
+        for i in range(ntest):
+            for c in range(self.nclasses):
+                pc[i, c] = np.log(self.class_priors[c])
+                for j in range(self.nfeatures):
+                    if features[i,j] == 1:
+                        pc[i,c] += np.log(self.likelihood[c,j])
+                    else:
+                        pc[i,c] += np.log(1 - self.likelihood[c,j])
+        
+        pc[i,c] = np.exp(pc[i,c])
+        prediction = np.argmax(pc, axis=-1)
+        
+        correct, false = 0, 0
+        for i in range(ntest):
+            if prediction[i] == labels[i]:
+                correct += 1
+            else:
+                false +=1
 
+        print(correct/ntest)
+        print(false/ntest)
 
 #%%
 import pandas as pd
 import numpy as np
+
 features=pd.read_csv('out_bag_of_words_5.csv', sep=',',header=None)
 labels = np.genfromtxt('out_classes_5.txt', delimiter='\n')
 
-np.unique(labels).shape[0]
+testfeatures = pd.read_csv('test_bag_of_words_0.csv', sep=',', header=None)
+testlabels = np.genfromtxt('test_classes_0.txt', delimiter='\n')
+
+
 
 #%%
 nb = NaiveBayes()
 nb.train(features,labels)
+nb.test(testfeatures, testlabels)
