86e086cee6edba657d877a49de4e0f9c25be2d8c
Cole Holcomb <cholcomb@astro.princeton.edu>
2018-02-26 16:55:21 -0500

    sentiments.py: J-Divergence feature filtering"

--- sentiments.py
+++ sentiments.py
@@ -53,7 +53,6 @@ class FeatureFilter(metaclass=ABCMeta):
         self.joint_nprob = self.terms_nprob - self.joint_ntprob
 
 
-
 class MutualInformation(FeatureFilter):
     '''
     Calculate the Mutual Information for a given set of vocabulary and classes.
@@ -104,21 +103,71 @@ class MutualInformation(FeatureFilter):
                         f.write('\n')
 
 
+class MaxDisc(FeatureFilter):
+    '''
+    Calculate the Maximally Discriminative features for a given set of vocabulary and classes.
+    
+    ref: https://arxiv.org/pdf/1602.02850.pdf
+    '''
+    def __init__(self, features, labels, vocab, n):
+        '''
+        Given a bag-of-words feature representation and associated class labels, 
+        calculate term probability, class probability, joint probability, and
+        J-Divergence for each term.
+
+        input from preprocessSentences.py:
+            features:   bag-of-words training matrix
+            labels:     class label vector
+            vocab:      vocab list
+            n:          number of features to reduce to
+
+        save to file:
+            reduced versions of inputs
+        '''
+        super().__init__(features, labels)
+        self.kl01  = self.joint_prob[0]*np.log2(self.joint_prob[0]/self.joint_prob[1])
+        self.kl10  = self.joint_prob[1]*np.log2(self.joint_prob[1]/self.joint_prob[0])
+        self.jd = self.kl01 + self.kl10
+
+        self.vocab = sorted([(x,y,z) for x,y,z in zip(range(self.nfeatures), vocab, self.jd)], 
+                            key=itemgetter(2), reverse=True)[:n]
+
+        self.vocabkl01 = sorted([(x,y,z) for x,y,z in zip(range(self.nfeatures), vocab, self.kl01)], 
+                            key=itemgetter(2), reverse=True)[:n]
+        self.vocabkl10 = sorted([(x,y,z) for x,y,z in zip(range(self.nfeatures), vocab, self.kl10)], 
+                            key=itemgetter(2), reverse=True)[:n]
+        
+        inds = np.array(self.vocab).T[0].astype(int)
+
+        #save ordered vocabulary list of n most informative words, with indicies and MI
+        with open('jd_'+str(n)+'_vocab_1.txt', 'w', encoding='utf-8') as f:
+            for l in self.vocab:
+                f.write(str(l[1])+'\n')
+
+        with open('jd_'+str(n)+'_bag_of_words_1.csv', 'w', encoding='utf-8') as f:
+            for i in range(self.nexamples):
+                for j in range(n):
+                    k = inds[j]
+                    f.write(str(self.features[i,k]))
+                    if j < n-1:
+                        f.write(',')
+                    else:
+                        f.write('\n')
 #%%
 vocab = open('out_vocab_1.txt', 'r', encoding='utf-8').read(-1).split('\n')
 #if '$' in vocab[0]:
 #    vocab[0] = '$'
-filt = MutualInformation(features, labels, vocab, 1621)
-filt.vocab
+filt = MutualInformation(features, labels, vocab, 20)
+for i in range(20):
+    print(filt.vocab[i])
+
 
 #%%
 inds = np.array(filt.vocab).T[0].astype(int)
 inds[0]
 
 #%%
-for i in vocab:
-    if i not in np.array(filt.vocab).T[1].astype(str):
-        print(i)
+filt.joint_prob[0]
 
 #%%
 import matplotlib.pyplot as plt
@@ -198,10 +247,25 @@ class NaiveBayes():
 import pandas as pd
 import numpy as np
 
+jd_thresholds = [10,50,100,200,300,400,500,1621]
 mi_thresholds = [10,50,100,200,300,400,500,1621]
 thresholds = [1]
 
+for i in jd_thresholds:
+    print('jd: '+str(i))
+    testfeatures = pd.read_csv('jd_'+str(i)+'_test_bag_of_words_0.csv', sep=',', header=None)
+    testlabels = np.genfromtxt('jd_'+str(i)+'_test_classes_0.txt', delimiter='\n')
+
+    features=pd.read_csv('jd_'+str(i)+'_bag_of_words_1.csv', sep=',',header=None)
+    labels = np.genfromtxt('out_classes_1.txt', delimiter='\n')
+
+    nb = NaiveBayes()
+    nb.train(features,labels)
+    nb.test(testfeatures, testlabels)
+    print('\n')
+
 for i in mi_thresholds:
+    print('mi: '+str(i))
     testfeatures = pd.read_csv('mi_'+str(i)+'_test_bag_of_words_0.csv', sep=',', header=None)
     testlabels = np.genfromtxt('mi_'+str(i)+'_test_classes_0.txt', delimiter='\n')
 
@@ -214,6 +278,7 @@ for i in mi_thresholds:
     print('\n')
 
 for i in thresholds:
+    print('all: '+str(i))
     testfeatures = pd.read_csv('test'+str(i)+'_bag_of_words_0.csv', sep=',', header=None)
     testlabels = np.genfromtxt('test'+str(i)+'_classes_0.txt', delimiter='\n')
 
